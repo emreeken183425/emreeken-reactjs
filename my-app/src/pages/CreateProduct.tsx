e6db1a42a5896f00608dad85f1e5db59eafe60ea
emreeken183425 <emreeken1834@gmail.com>
2022-10-05 21:08:04 +0300

    LAST

--- my-app/src/pages/CreateProduct.tsx
+++ my-app/src/pages/CreateProduct.tsx
@@ -41,7 +41,7 @@ const CreateProduct = () => {
           handleSubmit(e);
         }}
       >
-       <div className=" text-white gap-5 mt-5 ">
+       <div className=" text-white gap-5 m-5 ">
        <label htmlFor="name">Name:</label>
         <input
           type="text"
@@ -49,10 +49,11 @@ const CreateProduct = () => {
           onChange={(e) => {
             setName(e.target.value);
           }}
+          placeholder=' Enter your name'
           required
         />
        </div>
-       <div className=" text-white gap-5 mt-5 ">
+       <div className=" text-white gap-5 m-5 ">
        <label htmlFor="price">Price:</label>
         <input
           type="number"
@@ -60,13 +61,14 @@ const CreateProduct = () => {
           onChange={(e) => {
             setPrice(e.target.value);
           }}
+          placeholder=' Enter price'
           required
         />
        </div>
-        <div className=" text-white gap-5 mt-5 " >
-        <label htmlFor="category">category:</label>
-        {/* <input type="text" id="category" onChange={(e)=>{setCategory(e.target.value)}}required /> */}
-        <select name="category" id="category" onChange={(e)=>setCategory(e.target.value)} >
+        <div className=" text-white gap-5 m-5 " >
+        <label htmlFor="category">category:</label><br />
+       
+        <select  name="category"  placeholder=' choose category' id="category" onChange={(e)=>setCategory(e.target.value)} >
           <option value="Clothing">Clothing</option>
           <option value="Electronics">Electronics</option>
           <option value="Accessories">Accessories</option>
@@ -74,7 +76,7 @@ const CreateProduct = () => {
           <option value="Hobby">Hobby</option>
         </select>
         </div>
-        <div className=" text-white gap-5 mt-5 ">
+        <div className=" text-white gap-5 m-5 ">
         <label htmlFor="avatar">avatar:</label>
         <input
           type="text"
@@ -82,53 +84,31 @@ const CreateProduct = () => {
           onChange={(e) => {
             setAvatar(e.target.value);
           }}
+          placeholder=' Load avatar'
           required
         />
         </div>
-       <div className=" text-white gap-5 mt-5 ">
-       <label htmlFor="description">description:</label>
+       <div className=" text-white gap-5 m-5 ">
+       <label htmlFor="description" >description:</label>
         <input
           type="text"
           id="description"
           onChange={(e) => {
             setDesc(e.target.value);
           }}
+          placeholder=' Enter your description'
           required
         />
        </div>
-        <div className=" text-white gap-5 mt-5 ">
+        <div className=" text-white gap-5 m-5 ">
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
+        <input type="email" id="email" placeholder=' Enter your email' required />
         </div>
-        <div className=" text-white   ">
-        <button  className='m-2 btn btn-outline-light bg-warning ' type="submit">SUBMİT</button>
-        </div>
+       
       </form>
-
-      {/* <div  className='createproduct grid sm:grid-cols-2 lg:grid-cols-3 gap-5 ' >         
-             <div  className="card w-96 bg-base-100 shadow-xl  ">
-                  <figure >
-                      <img  className=" imgStyle max-h-80 w-52 " src={''}   alt={''}  />
-                  </figure>
-               <div className="card-body">
-               <div className="d-flex justify-around " >
-                 <h2 className="card-title justify-start"> name</h2>
-               <div className="badge badge-primary p-3 d-flex align-items-center ">price TL </div>
-               </div>
-              </div>
-          
-            <div className="card-actions justify-center mb-0 ">
-            <div className="badge badge-primary p-3 d-flex align-items-center ">description</div>
-          </div>
-          <div className="card-actions justify-center mb-0 ">
-            <div className="badge badge-primary p-3 d-flex align-items-center ">category</div>
-          </div>     
-    </div>
-    </div> */}
-      {/* 
-    <div className="justify-center  ">
-    <button className='m-1  btn btn-outline-light bg-primary ' >✅ CREATE</button>
-    </div> */}
+      <div className=" butonsubmit ">
+        <button  className='badge badge-primary bg-primary mt-3 p-3 d-flex align-items-center ' type="submit">SUBMİT</button>
+        </div>
     </div>
   );
 };
